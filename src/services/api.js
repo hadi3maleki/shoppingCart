9ec1d16dc0b3bc15dc0edb194c19c096bb6f2c7b
Hadis Maleki <hadi3.maleki@gmail.com>
2022-09-14 12:45:06 +0430

    Build a function to get information

---
+++ src/services/api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+const BASE_URL = "https://facestoreapi.com";
+const getProducts = async ()=>{
+    const response = await axios.get(`${BASE_URL}/Products`);
+    return response.data;
+}
+
+expect {getProducts}
