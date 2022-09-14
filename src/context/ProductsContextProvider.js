2b267b4ef5a66b4b16dd7ea14bedfbbe07ac57f8
Hadis Maleki <hadi3.maleki@gmail.com>
2022-09-14 14:03:34 +0430

    Creating a context for easier access to props

---
+++ src/context/ProductsContextProvider.js
@@ -0,0 +1,26 @@
+import React , { useEffect, useState, createContext } from 'react';
+//API
+import { getProducts } from '../services/api';
+
+export const ProductsContext = createContext();
+
+const ProductsContextProvider = (props) => {
+
+    const [products, setProducts] = useState([]);
+    
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setProducts(await getProducts());  
+        }
+
+        fetchAPI();
+    }, []);
+
+    return (
+        <ProductsContext.Provider value={products}>
+            {props.children}
+        </ProductsContext.Provider>
+    );
+};
+
+export default ProductsContextProvider;
