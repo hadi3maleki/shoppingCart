ac6b25aab3a7145c2cd8d04d6cb8eec40e37a429
Hadis Maleki <hadi3.maleki@gmail.com>
2022-09-14 18:29:08 +0430

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "https://facestoreapi.com";
+const BASE_URL = "https://fakestoreapi.com";
 const getProducts = async ()=>{
     const response = await axios.get(`${BASE_URL}/Products`);
     return response.data;
