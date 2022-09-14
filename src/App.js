48647a12b7a1208ae317abe0771ca6ec54402e0b
Hadis Maleki <hadi3.maleki@gmail.com>
2022-09-14 18:27:27 +0430

    Add component store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+
+//context
 import ProductsContextProvider from './context/ProductsContextProvider';
+
+//components
+import Store from './components/Store';
+
 const App = () => {
   return (
     <div>
       <ProductsContextProvider>
-
+        <Store/>
       </ProductsContextProvider>
     </div>
   );
