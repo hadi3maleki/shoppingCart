212ff7e1d5594f918cc583719a5ed42e371df73e
Hadis Maleki <hadi3.maleki@gmail.com>
2022-09-18 14:37:34 +0430

    Create CartContextProvider & Writing action ADD-ITEM and REMOVE-ITEM

---
+++ src/context/CartContextProvider.js
@@ -0,0 +1,42 @@
+import React, { useReducer } from 'react';
+
+const initialState = {
+    selectedItems : [],
+    itemsCounter : [],
+    total: 0,
+    checkOut: false
+}
+
+const cartReducer = (state, action)=>{
+    switch(action.type){
+        case "ADD_ITEM":
+            if(!state.selectedItems.find(item => item.id === action.payload.id )){
+                state.selectedItems.push({
+                    ...action.payload,
+                    quantity: 1
+                })
+            }
+            return {
+                ...state, selectedItems: [...state.selectedItems]
+            }
+        case "REMOVE_ITEM":
+            const newSelectedItems =  state.selectedItems.filter(item => item.id !== action.payload.id);
+            return{
+                ...state, selectedItems:[...newSelectedItems]
+            }
+    } 
+}
+
+
+const CartContextProvider = () => {
+
+    const [state, dispatch] = useReducer(cartReducer, initialState)
+    return (
+
+        <div>
+            
+        </div>
+    );
+};
+
+export default CartContextProvider;
