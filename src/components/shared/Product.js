dfde4a357d6679d9ecf23bcc21d37b7bd7ae67e4
Hadis Maleki <hadi3.maleki@gmail.com>
2022-09-19 13:31:41 +0430

    using trashIcon in product.js file & Sort codes

--- src/components/shared/Product.js
+++ src/components/shared/Product.js
@@ -4,6 +4,8 @@ import { shorten, isInCart, quantityCount } from '../../helper/functions';
 
 import { CartContext } from '../../context/CartContextProvider';
 
+import trashIcon from "../../assets/icons/trash.svg";
+
 const Product = ({productData}) => {
     const {state, dispatch} = useContext(CartContext);
     return (
@@ -15,14 +17,25 @@ const Product = ({productData}) => {
                 <Link to={`/products/${productData.id}`}>Detaile</Link>
                 <div>
                     {
+                        quantityCount(state, productData.id) > 1 
+                        && 
+                        <button onClick={()=>dispatch({type:"DECREASE", payload: productData})}>-</button>
+                    }
+                    {
+                        quantityCount(state, productData.id) === 1 
+                        && 
+                        <button onClick={()=>dispatch({type:"REMOVE_ITEM", payload: productData})}><img src={trashIcon} alt = "trash" style={{width : "20px"}}/></button>
+                    }
+                    
+                    
+                    {
                         isInCart(state, productData.id) ? 
                         <button onClick={()=>dispatch({type:"INCREASE", payload: productData})}> + </button>
                         :
                         <button onClick={()=>dispatch({type:"ADD_ITEM", payload: productData})}> Add to cart </button>
                     }
                     
-                    {quantityCount(state, productData.id) > 1 && <button onClick={()=>dispatch({type:"DECREASE", payload: productData})}>-</button>}
-                    {quantityCount(state, productData.id) === 1 && <button onClick={()=>dispatch({type:"REMOVE_ITEM", payload: productData})}>REMOVE</button>}
+                    
                     
 
                 </div>
